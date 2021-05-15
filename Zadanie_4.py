c5da6e31d5bff997d60bb537b8a7019b6da4f530
Manuela-Kobusiak <82751527+Manuela-Kobusiak@users.noreply.github.com>
2021-05-15 10:17:40 +0200

    Update Zadanie_4.py

--- Zadanie_4.py
+++ Zadanie_4.py
@@ -7,11 +7,8 @@ Created on Sat May 15 08:38:08 2021
 
 lista=[2,7,5,6,4,8,3,1]
 n=len(lista)
-print(n)
 for i in range(0,n-1):
-    print("i:",i)
     for j in range(i+1,n):
-        print("j:",j)
         if lista[i]>lista[j]:
             maks=lista[i]
             lista[i]=lista[j]
